745c7d4a8596f0524967856374104663f927f162
siddharth301 <74924743+siddharth301@users.noreply.github.com>
2021-01-26 21:28:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var contestantCount, database, quiz, question, contestant;
+var gameState = 0;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
